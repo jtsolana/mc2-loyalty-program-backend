20bbb7de6530cf1c657113ef199618802ea00cf4
Junard Oclarit <jtoclarit@gmail.com>
2026-05-13 22:53:20 +0800

    Update customer details in loyverse upon social login.

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -102,8 +102,9 @@ class AuthService
             LoyaltyPoint::create(['user_id' => $user->id]);
 
             $loyverseId = $this->loyverseService->createCustomer([
-                'name' => $user->name,
-                'email' => $user->email,
+                'name' => '---------------------',
+                'email' => Str::lower(Str::toBase64($user->email)).'@mc2.com',
+                'phone' => '00000000000',
                 'customer_code' => $user->hashed_id,
             ]);
 
